0bb26627561fa01dd2220616e0055c5f9ca95174
Devansh <devanshtayal2005@gmail.com>
2020-06-17 02:58:06 +0530

    Comments

--- dic.py
+++ dic.py
@@ -1,15 +1,15 @@
 import json
 from difflib import get_close_matches
 
-file=json.load(open("original.json","r"))
-word=input("Enter the word : \n")
-word= word.lower()
+file=json.load(open("original.json","r"))  #read the json file
+word=input("Enter the word : \n")      #word to be searched
+word= word.lower()                
 def dic_word(words):
     if word in file:
-        return file[word]
+        return file[word]                   #key se value milli 
 
     elif word.title() in file:
-        return file[word.title()]
+        return file[word.title()]            
 
     elif( word.capitalize() in file):
         return file[word.capitalize()]
@@ -17,8 +17,8 @@ def dic_word(words):
     elif word.upper() in file:
         return file[word.upper()]
 
-    elif len(get_close_matches(word,file.keys())) > 0:
-        print("Did you mean %s instead" %get_close_matches(word,file.keys())[0])
+    elif len(get_close_matches(word,file.keys())) > 0:                                # close match word
+        print("Did you mean %s instead" %get_close_matches(word,file.keys())[0])      #cutoff argument is not present
         decide=input("Press y for Yes and n for NO \n")
         if decide =="y" or "Y":
             return file[get_close_matches(word,file.keys())[0]]
@@ -31,10 +31,10 @@ def dic_word(words):
         print("We dont have this word in the dictionary")
 
 output= dic_word(word)
-if type(output)==list:
-    for item in output:
+if type(output)==list:          
+    for item in output:                #if the word has two meaning return a string not a list
  
         print(item)
-else:
+else:                                       # word has only one meaning
 
     print(output)
